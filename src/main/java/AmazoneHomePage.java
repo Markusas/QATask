409592608eec5ff44f10b4acd41fbf440d33ba3c
Markusas <saltinio@gmail.com>
2021-12-30 15:37:37 +0200

    Added implementation for home and sign-in pages

--- src/main/java/AmazoneHomePage.java
+++ src/main/java/AmazoneHomePage.java
@@ -1,2 +1,7 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class AmazoneHomePage {
+    WebDriver driver;
+    By signInButton = By.xpath("//a[@id='nav-link-accountList']");
 }
--- src/main/java/SignInPage.java
+++ src/main/java/SignInPage.java
@@ -1,2 +1,10 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class SignInPage {
+    WebDriver driver;
+    By email = By.id("ap_email");
+    By continueButton = By.id("continue");
+    By password = By.id("ap_password");
+    By signInSubmit = By.id("signInSubmit");
 }
