1ae1f7ed5c5a9309e872e9769ff71027a9682248
Markusas <saltinio@gmail.com>
2022-01-03 09:32:05 +0200

    Added constructors

--- src/main/java/com/amazone/pages/AmazoneHomePage.java
+++ src/main/java/com/amazone/pages/AmazoneHomePage.java
@@ -4,6 +4,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class AmazoneHomePage {
+    public AmazoneHomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
     WebDriver driver;
     By signInButton = By.xpath("//a[@id='nav-link-accountList']");
 }
--- src/main/java/com/amazone/pages/SignInPage.java
+++ src/main/java/com/amazone/pages/SignInPage.java
@@ -4,6 +4,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class SignInPage {
+    public SignInPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
     WebDriver driver;
     By email = By.id("ap_email");
     By continueButton = By.id("continue");
