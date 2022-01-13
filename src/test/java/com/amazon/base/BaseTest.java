22456c997214ab1da99aba566d3a664854ca3fab
Markusas <saltinio@gmail.com>
2022-01-13 10:06:28 +0200

    Testing git action

--- src/test/java/com/amazon/base/BaseTest.java
+++ src/test/java/com/amazon/base/BaseTest.java
@@ -3,7 +3,6 @@ package com.amazon.base;
 import com.amazon.pageObjects.CommonElements;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.*;
